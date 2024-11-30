9e4cdca3e8e6047c6170fc4e79f99b04296cb107
Hardik Rathod <hardikrathod1219@gmail.com>
2024-11-30 17:25:20 +0530

    feat(Companies section ): animations added

--- src/sections/Companies.tsx
+++ src/sections/Companies.tsx
@@ -1,4 +1,6 @@
+"use client";
 import { SectionContent } from "@/components/SectionContent";
+import { motion } from "framer-motion";
 import AcmeCorpLogo from "../assets/images/acme-corp-logo.svg";
 import CelestialLogo from "../assets/images/celestial-logo.svg";
 import EchoValleyLogo from "../assets/images/echo-valley-logo.svg";
@@ -41,13 +43,28 @@ export const Companies = () => {
                     Empowering creators at leading companies
                 </h2>
                 <div className="-mx-4 mt-20 flex overflow-x-clip lg:-mx-8">
-                    <div className="flex flex-none gap-18 px-18 md:gap-36">
-                        {companies.map(({ logo: Logo, name }) => (
-                            <div key={name}>
-                                <Logo className="h-8" />
-                            </div>
-                        ))}
-                    </div>
+                    <motion.div
+                        className="flex flex-none gap-18 px-9 md:gap-36 md:px-18"
+                        initial={{
+                            x: 0,
+                        }}
+                        animate={{
+                            x: "-50%",
+                        }}
+                        transition={{
+                            duration: 10,
+                            repeat: Infinity,
+                            ease: "linear",
+                        }}
+                    >
+                        {[...companies, ...companies].map(
+                            ({ logo: Logo }, index) => (
+                                <div key={index}>
+                                    <Logo className="h-8" />
+                                </div>
+                            ),
+                        )}
+                    </motion.div>
                 </div>
             </SectionContent>
         </section>
