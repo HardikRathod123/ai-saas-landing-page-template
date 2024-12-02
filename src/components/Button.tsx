85e6d30e490582cc78f970c40d0ecd52349d10d8
Hardik Rathod <hardikrathod1219@gmail.com>
2024-12-02 13:37:42 +0530

    feat(UI): Button animation added

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,14 @@
+"use client";
 import { cn } from "@/lib/utils";
 import { cva, VariantProps } from "class-variance-authority";
-import { ButtonHTMLAttributes, forwardRef } from "react";
+import {
+    animate,
+    HTMLMotionProps,
+    motion,
+    useMotionTemplate,
+    useMotionValue,
+} from "framer-motion";
+import { forwardRef, useEffect, useState } from "react";
 
 const buttonVariants = cva(
     "h-10 px-6 text-xs font-bold uppercase tracking-widest rounded-lg",
@@ -22,19 +30,49 @@ const buttonVariants = cva(
 );
 
 export interface ButtonProps
-    extends ButtonHTMLAttributes<HTMLButtonElement>,
+    extends HTMLMotionProps<"button">,
         VariantProps<typeof buttonVariants> {}
 
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-    ({ className = "", block, variant, children, ...props }, ref) => {
+    (
+        { className = "", block, variant = "primary", children, ...props },
+        ref,
+    ) => {
+        const [isHovered, setIsHovered] = useState(false);
+        const angle = useMotionValue(45);
+        const background = useMotionTemplate`linear-gradient(var(--color-gray-950),var(--color-gray-950)) padding-box, conic-gradient(from ${angle}deg,var(--color-violet-400),var(--color-fuchsia-400),var(--color-amber-300),var(--color-teal-300)) border-box`;
+
+        useEffect(() => {
+            if (isHovered) {
+                animate(angle, angle.get() + 360, {
+                    duration: 1,
+                    repeat: Infinity,
+                    ease: "linear",
+                });
+            } else {
+                animate(angle, 45, { duration: 0.5 });
+            }
+        }, [isHovered, angle]);
+
         return (
-            <button
+            <motion.button
                 ref={ref}
                 className={cn(buttonVariants({ className, variant, block }))}
+                onMouseEnter={() => {
+                    setIsHovered(true);
+                }}
+                onMouseLeave={() => setIsHovered(false)}
+                style={
+                    variant === "primary"
+                        ? {
+                              background,
+                          }
+                        : undefined
+                }
                 {...props}
             >
                 {children}
-            </button>
+            </motion.button>
         );
     },
 );
